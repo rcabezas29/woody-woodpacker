b2ddc3d630175360062cc9737cbc8292d1e3544f
Manuel Martin lopez <manmarti@c3r12s1.42madrid.com>
2024-09-16 19:44:58 +0200

    Temp: find code cave

--- src/main.c
+++ src/main.c
@@ -43,6 +43,39 @@ unsigned char *generate_key(void)
 	return key;
 }
 
+uint64_t find_code_cave(uint8_t *buffer, uint64_t size)
+{
+	uint64_t current_best = 0;
+	uint64_t max_size = 0, i = 0;
+	while (i < size)
+	{
+		if (buffer[i] != 0)
+		{
+			i++;
+			continue;
+		}
+		uint64_t current_size = 0, j = i;
+		while (j < size && buffer[j] == 0x00)
+		{
+			++current_size;
+			++j;
+		}
+		if (current_size > max_size)
+		{
+			current_best = i;
+			max_size = current_size;
+		}
+		i = j;
+	}
+	
+	// for (uint64_t i = 0; i < size; ++i)
+	// {
+
+	// }
+	printf("Cave size = %li\n", max_size);
+	return current_best;
+}
+
 
 void	print_usage(void)
 {
@@ -61,7 +94,7 @@ int	main(int argc, char **argv)
 	if (key == NULL)
 		return -1;
 
-	woody_fd = open("./woody", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+	woody_fd = open("./woody", O_CREAT |  O_TRUNC | O_RDWR, S_IRWXU);
 	if (woody_fd == -1)
 		return 1;
 	progfd = open(argv[1], O_RDONLY);
@@ -87,16 +120,27 @@ int	main(int argc, char **argv)
 
 	char	buffer[64];
 	int		ret;
+	lseek(progfd, 0, SEEK_SET);
 	while ((ret = read(progfd, buffer, 64)) > 0)
 	{
-		for (int i = 0; i < ret; ++i)
-			buffer[i] ^= key[i];
+		// for (int i = 0; i < ret; ++i)
+		// 	buffer[i] ^= key[i];
 		write(woody_fd, buffer, ret);
 	}
 	printf("key_value: %s\n", key);
 
+
+	int size = lseek(woody_fd, 0, SEEK_END);
+	uint8_t *cave_buffer = malloc(size);
+	lseek(woody_fd, 0, SEEK_SET);
+	read(woody_fd, cave_buffer, size);
+
+	uint64_t code_cave_dir = find_code_cave(cave_buffer, size);
+	printf("Code cave dir = 0x%lx\n", code_cave_dir);
+
 	free(key);
 	free(file);
+	free(cave_buffer);
 	close(woody_fd);
 	close(progfd);
 	return 0;
