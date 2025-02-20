e2d19685035aa7afed871e3c4008de318503ad8b
rcabezas29 <raul.29cg@gmail.com>
2025-02-20 23:01:01 +0100

    Getting text section and fixed key storage in assembly

--- payload_generator/decryptor.s
+++ payload_generator/decryptor.s
@@ -1,4 +1,5 @@
 %define KEY_SIZE 64
+%define WOODY_STACK_SIZE 78
 
 %define SYS_READ 0
 %define SYS_WRITE 1
@@ -12,52 +13,54 @@ section .text
 global _start
 
 _start:
-    push rax
-    push rdi
-    push rsi
     push rdx
+    push rsp
 
-    push rbp
-    mov rbp, rsp
-    sub rsp, 0x20
+    sub rsp, WOODY_STACK_SIZE
+    mov r15, rsp
 
 _print_woody:
-    mov qword [rsp], 0x2E2E2E2E
-    mov qword [rsp + 4], 0x444F4F57
-    mov qword [rsp + 8], 0x2E2E2E59
-    mov word [rsp + 12], 0x0A2E
+    mov qword [r15], 0x2E2E2E2E
+    mov qword [r15 + 4], 0x444F4F57
+    mov qword [r15 + 8], 0x2E2E2E59
+    mov word [r15 + 12], 0x0A2E
 
     mov rax, 1
     mov rdi, 1
-    mov rsi, rsp
+    mov rsi, r15
     mov rdx, 14
     syscall
 
 _ask_key:
-    sub rsp, 10
-    mov qword [rsp], 0x73736150
-    mov qword [rsp + 4], 0x64726F77
-    mov word [rsp + 8], 0x203A
+    mov qword [r15], 0x73736150
+    mov qword [r15 + 4], 0x64726F77
+    mov word [r15 + 8], 0x203A
 
     mov rax, SYS_WRITE
     mov rdi, STDOUT
-    mov rsi, rsp
+    mov rsi, r15
     mov rdx, 10
     syscall
 
 _read_input_key:
-    sub rsp, 64
     mov rax, SYS_READ
     mov rdi, STDIN
-    mov rsi, key   ; buffer to store input
+    lea rsi, [r15 + 14]   ; buffer to store input
     mov rdx, 64    ; number of bytes to read
     syscall
 
-_start_decrypt:
-    mov rsi, [text_start]  ; Address of .text section
-    mov rdx, [text_size]   ; Size of .text section
-    mov rdi, key           ; Address of key
-    mov rcx, 64            ; Size of key
+_get_text_section_addr:
+    call .delta
+    .delta:
+        pop rbp
+        sub rbp, .delta
+    lea rsi, [rbp + _start] ; (- TEXT SIZE) Save in rsi .text address
+
+; _start_decrypt:
+;     mov rsi, [text_start]  ; Address of .text section
+;     mov rdx, [text_size]   ; Size of .text section
+;     mov rdi, key           ; Address of key
+;     mov rcx, 64            ; Size of key
 
 ; decrypt_loop:
 ;     mov r8b, byte [rsi]
@@ -75,19 +78,8 @@ _start_decrypt:
 ;     jmp decrypt_loop
 
 decrypt_done:
-    mov rsp, rbp
-    pop rbp
-
+    add rsp, WOODY_STACK_SIZE
+    pop rsp
     pop rdx
-    pop rsi
-    pop rdi
-    pop rax
-
-    jmp _end
-
-_variables:
-text_start: times 8 nop    ; variable to store the start address of .text section
-text_size: times 8 nop     ; variable to store the size of .text section
-key: times 64 nop  ; buffer to store the key
 
 _end:
