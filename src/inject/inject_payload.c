f72a65d76f5d8098b23065bfc609b27fb78bc5bb
Manmartin <manuelmartinlopez02@gmail.com>
2025-03-24 23:09:20 +0100

    find_value refactor

--- src/inject/inject_payload.c
+++ src/inject/inject_payload.c
@@ -1,11 +1,11 @@
 #include <woody-woodpacker.h>
 #include <melf.h>
 
-static unsigned char *find_value(unsigned char *haystack, uint64_t needle)
+static uint64_t *find_value(uint8_t *haystack, uint64_t needle)
 {
     while (*(uint64_t *) haystack != needle)
         ++haystack;
-    return haystack;
+    return (uint64_t *)haystack;
 }
 
 woody_status inject_payload(unsigned char *file, uint64_t file_size, payload_t payload, uint64_t key_size)
@@ -29,11 +29,11 @@ woody_status inject_payload(unsigned char *file, uint64_t file_size, payload_t p
     payload_segment->file_size = payload.size;
     payload_segment->memory_size = payload.size;
 
-    *((uint64_t *) find_value(payload.value, OEP)) = file_header->entry_point;
-    *((uint64_t *) find_value(payload.value, OVA)) = text_segment->virtual_address;
-    *((uint64_t *) find_value(payload.value, NEP)) = payload_segment->virtual_address;
-    *((uint64_t *) find_value(payload.value, CSZ)) = text_segment->memory_size;
-    *((uint64_t *) find_value(payload.value, KSZ)) = key_size;
+    *find_value(payload.value, OEP) = file_header->entry_point;
+    *find_value(payload.value, OVA) = text_segment->virtual_address;
+    *find_value(payload.value, NEP) = payload_segment->virtual_address;
+    *find_value(payload.value, CSZ) = text_segment->memory_size;
+    *find_value(payload.value, KSZ) = key_size;
 
     file_header->entry_point = payload_segment->virtual_address;
     ft_memmove(file + file_size, payload.value, payload.size);
